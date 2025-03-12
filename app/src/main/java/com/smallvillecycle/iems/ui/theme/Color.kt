21a86d718b64807d7c3814a293f83d707e999260
smallville <abutimartin778@gmail.com>
2025-03-12 10:03:29 +0300

    c1

---
+++ app/src/main/java/com/smallvillecycle/iems/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.smallvillecycle.iems.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Green80 = Color(0xFFA7DCA5)    // Soft Green
+val GreenGrey80 = Color(0xFFB8CBB8) // Muted Green-Grey
+val Lime80 = Color(0xFFDAF7A6)     // Light Lime Green
+
+val Green40 = Color(0xFF2E7D32)    // Deep Green
+val GreenGrey40 = Color(0xFF4B6256) // Dark Green-Grey
+val Lime40 = Color(0xFF9ACD32)     // Olive Green
